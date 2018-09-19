a0fc03e6a212e341b3cd25dc07f2b49b55521b42
Dorien Foppen-de Graaf <dorien.degraaf@student.hu.nl>
2018-09-19 19:27:18 +0200

    TEST

---
+++ Les04/pe4.3.py
@@ -0,0 +1,4 @@
+loon = float(input('Wat verdien je per uur: '))
+hoeveel = float(input('Hoeveel uur heb je gewerkt: '))
+totaal = loon * hoeveel
+print(str(hoeveel) + ' uur werken levert ' + str(totaal) + ' Euro op')
