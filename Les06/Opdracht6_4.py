06b58ff1d5094f2c144d0b3af5e90f5e5e5c9011
Dorien Foppen-de Graaf <dorien.degraaf@student.hu.nl>
2018-09-23 16:13:00 +0200

    Huiswerk met extra optie

--- Les06/Opdracht6_4.py
+++ Les06/Opdracht6_4.py
@@ -1,5 +1,13 @@
 def new_password(oldpassword, newpassword):
-    if newpassword != oldpassword and len(newpassword)>= 6:
+    cijfer = '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+
+    getalgevonden = False
+
+    for getal in cijfer:
+        if getal in newpassword:
+            getalgevonden = True
+
+    if newpassword != oldpassword and len(newpassword) >= 6 and getalgevonden == True:
         return True
     else:
         return False
